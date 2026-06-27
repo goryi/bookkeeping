6f558014adfa6c133a6b5cdd40091e4b20414afe
goryi <51978990+goryi@users.noreply.github.com>
2026-06-27 13:54:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'nf-v5';
+
+self.addEventListener('install', e => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        fetch(e.request).then(r => {
+            if (r.ok) {
+                const clone = r.clone();
+                caches.open(CACHE).then(c => c.put(e.request, clone));
+            }
+            return r;
+        }).catch(() => caches.match(e.request))
+    );
+});
